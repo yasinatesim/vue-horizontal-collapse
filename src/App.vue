3f26ec5cbf4284474b43b8590b7282ccfc632a03
Yasin ATEŞ <yasinatesim@gmail.com>
2019-07-15 12:04:37 +0300

    Added Github Corner badge

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
+    <GithubCorner />
     <HorizontalCollapse />
   </div>
 </template>
 
 <script>
 import HorizontalCollapse from "./components/HorizontalCollapse";
+import GithubCorner from "./components/GithubCorner";
 
 export default {
   name: "app",
   components: {
-    HorizontalCollapse
+    HorizontalCollapse,
+    GithubCorner
   }
 };
 </script>
